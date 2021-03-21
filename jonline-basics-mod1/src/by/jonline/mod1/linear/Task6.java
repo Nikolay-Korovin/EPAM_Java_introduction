d6534eb0eea326580d103eaacded3acf5e215b7d
Nikolay-Korovin <61452948+Nikolay-Korovin@users.noreply.github.com>
2021-03-21 08:50:18 +0300

    secont rep
    yep

--- jonline-basics-mod1/src/by/jonline/mod1/linear/Task6.java
+++ jonline-basics-mod1/src/by/jonline/mod1/linear/Task6.java
@@ -10,13 +10,13 @@ public class Task6 {
 		double x = 0;
 		double y = -1;
 
-		if ((x >= 0 & x <= 2) & (y >= 0 & y <= 4)) // правоверх
+		if ((x >= 0 & x <= 2) & (y >= 0 & y <= 4)) 
 			System.out.println("true");
-		else if ((x >= 0 & x <= 4) & (y <= 0 & y >= -3)) // правониз
+		else if ((x >= 0 & x <= 4) & (y <= 0 & y >= -3)) 
 			System.out.println("true");
-		else if ((x <= 0 & x >= -4) & (y <= 0 & y >= -3)) // левониз
+		else if ((x <= 0 & x >= -4) & (y <= 0 & y >= -3)) 
 			System.out.println("true");
-		else if ((x <= 0 & x >= -4) & (y >= 0 & y <= 4)) // левоверх
+		else if ((x <= 0 & x >= -4) & (y >= 0 & y <= 4)) 
 			System.out.println("true");
 		else
 			System.out.println("false");
